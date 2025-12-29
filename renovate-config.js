a7d9b2ab8af8e88786a56148da518265040bedc0
bagas <bagas@fossy.my.id>
2025-12-29 17:15:42 +0700

    update: renovate target pr to staging branch

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "endpoint": "https://git.fossy.my.id/api/v1",
-    "gitAuthor": "Renovate Bot <renovate-bot@fossy.my.id>",
+    "gitAuthor": "Renovate-Clanker <renovate-bot@fossy.my.id>",
     "platform": "gitea",
     "onboardingConfigFileName": "renovate.json",
     "autodiscover": true,
